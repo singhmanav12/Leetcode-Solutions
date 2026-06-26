84bf5edccbf14da208aa07014c5c399000f8d9ae
Manavendra Pratap Singh <mps.dbrg@gmail.com>
2026-06-26 13:49:10 +0530

    Time: 10 ms (92.84%), Space: 12.5 MB (91.86%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        left =0
+        right=len(s)-1
+        s=s.lower()
+        while left < right :
+            while left < right and not s[left].isalnum() :
+                left += 1
+            while left < right and not s[right].isalnum() :
+                right -= 1
+            if s[left] != s[right] :
+                return False
+            else :
+                left += 1
+                right -=1
+        return True
