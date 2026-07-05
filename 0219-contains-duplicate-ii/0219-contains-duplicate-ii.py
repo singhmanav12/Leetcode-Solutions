6f9e98133bb4b6ee0afe677138976a2301ecac55
Manavendra Pratap Singh <mps.dbrg@gmail.com>
2026-07-05 23:54:26 +0530

    Time: 59 ms (15.12%), Space: 24.5 MB (61.02%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        window = set()
+        
+        for i, num in enumerate(nums):
+            if num in window:
+                return True  
+            window.add(num)
+            if len(window) > k:
+                window.remove(nums[i - k])
+                
+        return False
